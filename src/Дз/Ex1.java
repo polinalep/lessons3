4ee733937202fbc987ee293ae60ba4fb016b1812
Bobrovich Polina <polinaleper542@gmail.com>
2022-11-29 18:37:12 +0300

    nas

--- src/Дз/Ex1.java
+++ src/Дз/Ex1.java
@@ -1,7 +1,25 @@
 package Дз;
 
+import java.util.Scanner;
+
 public class Ex1 {
     public static void main(String[] args){
+        int a = 1;
+        int b = 1;
+        int n;
+        int t;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        for(int i = 0; i < n; i++){
+
+        }
+            t = a + b;
+            a = b;
+            b = t;
+            System.out.print(t + " ");
 
     }
+
+
+
 }
